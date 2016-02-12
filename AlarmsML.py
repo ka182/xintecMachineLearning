741e4dc617054210524c21af699afdcc21423ad4
Carmine <calicchiocarmine@gmail.com>
2016-02-12 22:50:38 +0000

    ver2

--- AlarmsML.py
+++ AlarmsML.py
@@ -1,7 +1,6 @@
 import numpy as np
-from sklearn.naive_bayes import GaussianNB
 
-data = open('alarms1.txt')
+data = open('5dAlarms.txt')
 
 count = 0
 
@@ -9,10 +8,9 @@ X1=[]
 Y1=[]
 
 for line in data:
-    xx,yy,y = line.split(',')
-    X1 = "%s,[%s,%s]" % (X1,xx,yy)
+    xx,yy,zz,ww,uu,y = line.split(',')
+    X1 = "%s,[%s,%s,%s,%s,%s]" % (X1,xx,yy,zz,ww,uu)
     Y1 = "%s,%s" % (Y1,y.strip())
 print(Y1)
-
 
 
